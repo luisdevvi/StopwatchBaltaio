bd4e5af5c15ca44068881872350b39233e15b1a0
luisdevvi <luis.devvi@gmail.com>
2024-10-30 20:48:42 -0300

    Add methods to get user input

--- StopwatchBaltaio/Program.cs
+++ StopwatchBaltaio/Program.cs
@@ -10,10 +10,14 @@
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Seconds");
-            Console.WriteLine("M = Minutes");
-            Console.WriteLine("0s = Exit");
-            Console.WriteLine("\nHow long do you want to count ?");
+            Console.WriteLine("S = Seconds => 10s = 10 seconds");
+            Console.WriteLine("M = Minutes => 1m = 1 minute");
+            Console.WriteLine("0 = Exit");
+            Console.WriteLine("\nHow long do you want to count ?\n");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
         }
 
         static void Start(int time)
