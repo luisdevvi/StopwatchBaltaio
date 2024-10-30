7b7e937212bb718748b70d7930e2d33fcf882abc
luisdevvi <luis.devvi@gmail.com>
2024-10-30 20:10:29 -0300

    Add project files.

---
+++ StopwatchBaltaio/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StopwatchBaltaio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+    }
+}
