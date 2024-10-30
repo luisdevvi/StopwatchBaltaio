1fa6d268cc7c9708923c5d8ce6c247fcdc02d59e
luisdevvi <luis.devvi@gmail.com>
2024-10-30 20:52:35 -0300

    Add time calculator and if instruction determine between seconds or minutes

--- StopwatchBaltaio/Program.cs
+++ StopwatchBaltaio/Program.cs
@@ -18,6 +18,17 @@
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
             int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                Environment.Exit(0);
+
+            Start(time * multiplier);
+
         }
 
         static void Start(int time)
